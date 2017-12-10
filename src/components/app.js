62b1a54613f13e6deb77fcb41bf5b8b2f560e850
Shoblik <shoblik@yahoo.com>
2017-12-09 19:21:57 -0800

    created the Stopwatch class but its throwing an error

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react'
+import Stopwatch from './stopwatch';
 
+const App = () => {
+    return(
+        <div>
+            <h1>Welcome to React MotherYaker!</h1>
+            <Stopwatch/>
+        </div>
+    )
+}
 
+export default App;
